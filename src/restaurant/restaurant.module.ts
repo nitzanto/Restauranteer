938c8acbdcd3fb4f5f818759fbf80dee587811e7
nitzanto <nitzan16001@gmail.com>
2023-12-09 18:10:58 +0200

    * TypeORM Entity

--- src/restaurant/restaurant.module.ts
+++ src/restaurant/restaurant.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { RestaurantController } from './restaurant.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Restaurant])],
   controllers: [RestaurantController],
   providers: [RestaurantService],
 })
