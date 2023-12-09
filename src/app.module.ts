5ea0e7e7a3010360a12f6d6712a86470a4c37fd8
nitzanto <nitzan16001@gmail.com>
2023-12-09 17:17:53 +0200

    * Created an entity for DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { RestaurantModule } from './restaurant/restaurant.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RestaurantModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DATABASE,
+      ssl: true,
+      autoLoadEntities: true,
+      synchronize: true, // need to set for false for production
+    }),
+    RestaurantModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/restaurant/entities/restaurant.entity.ts
+++ src/restaurant/entities/restaurant.entity.ts
@@ -1,1 +1,33 @@
-export class Restaurant {}
+import { Column, Entity, PrimaryGeneratedColumn, PrimaryColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity()
+export class Restaurant {
+  @PrimaryColumn('uuid')
+  id: string = uuidv4();
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  imgURL: string;
+
+  @Column({ type: 'double precision' }) //  for storing floating-point numbers like ratings
+  rating: number;
+
+  @Column()
+  location: string;
+
+  @Column()
+  cuisine: string;
+
+  @Column('timestamptz', { array: true }) // multiple reservation dates
+  reservationDates: Date[];
+
+  constructor(partial: Partial<Restaurant>) {
+    Object.assign(this, partial);
+  }
+}
