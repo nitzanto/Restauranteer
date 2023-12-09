3e67375c7acb04ed1a156bcb414ea9bebd7e2106
nitzanto <nitzan16001@gmail.com>
2023-12-09 17:03:40 +0200

    * Created REST API for restaurant

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [],
+  imports: [RestaurantModule],
   controllers: [],
   providers: [],
 })
