0cdf293512e8006e4896b14fe17f16ddb772ecd7
nitzanto <nitzan16001@gmail.com>
2023-12-10 21:28:05 +0200

    * Updated CORS

--- src/lambda.ts
+++ src/lambda.ts
@@ -8,7 +8,12 @@ export const handler = async (event, context) => {
   if (!cachedServer) {
     const nestApp = await NestFactory.create(AppModule);
     nestApp.enableCors({
-      origin: ['http://localhost:5173', 'https://bookify-sand.vercel.app/'],
+      origin: [
+        'http://localhost:5173',
+        'https://bookify-sand.vercel.app',
+        'https://bookify-9tw29quy8-nitzanto.vercel.app',
+        'https://bookify-nitzanto.vercel.app',
+      ],
     });
     await nestApp.init();
     cachedServer = serverlessExpress({
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://bookify-sand.vercel.app/'],
+    origin: [
+      'http://localhost:5173',
+      'https://bookify-sand.vercel.app',
+      'https://bookify-9tw29quy8-nitzanto.vercel.app',
+      'https://bookify-nitzanto.vercel.app',
+    ],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
